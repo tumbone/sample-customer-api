a475a5d31c1cd06a1572c0df03df67194e2e9e73
tumbonea <tumbone@example.com>
2024-06-12 21:41:50 +0200

    Added ECS service

--- infra/main.tf
+++ infra/main.tf
@@ -12,6 +12,34 @@ locals {
 # AWS VPC #
 ###########
 
+resource "aws_security_group" "ecs_sg" {
+  name        = "${local.naming_prefix}-sg"
+  description = "Security Group for ECS"
+  vpc_id      = "vpc-0cd43b3f77c7d9126"
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_security_group_rule" "ecs_sg_outbound_rule" {
+  cidr_blocks       = ["0.0.0.0/0"]
+  description       = "Allow internet egress"
+  from_port         = 0
+  protocol          = -1
+  security_group_id = aws_security_group.ecs_sg.id
+  to_port           = 0
+  type              = "egress"
+}
+
+resource "aws_security_group_rule" "ecs_sg_inbound_rule" {
+  cidr_blocks       = ["0.0.0.0/0"]
+  description       = "Allow tcp inbound"
+  from_port         = var.host_port
+  protocol          = -1
+  security_group_id = aws_security_group.ecs_sg.id
+  to_port           = var.host_port
+  type              = "ingress"
+}
 
 ###############
 # ECS CLUSTER #
@@ -28,6 +56,19 @@ data "aws_iam_policy_document" "ecs_execution_role_assume_policy" {
   }
 }
 
+data "aws_iam_policy_document" "ecs_execution_role_logs_policy" {
+  statement {
+    actions   = ["logs:CreateLogGroup", "logs:CreateLogStream", "logs:PutLogEvents"]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "ecs_execution_role_logs_policy" {
+  name   = "${local.naming_prefix}-ecs-logs-policy"
+  policy = data.aws_iam_policy_document.ecs_execution_role_logs_policy.json
+}
+
+
 resource "aws_iam_role" "ecs_execution_role" {
   name               = "${local.naming_prefix}-ecs-task-execution-role"
   assume_role_policy = data.aws_iam_policy_document.ecs_execution_role_assume_policy.json
@@ -36,6 +77,11 @@ resource "aws_iam_role" "ecs_execution_role" {
 resource "aws_iam_role_policy_attachment" "ecs_execution_role_assume_policy_attachment" {
   role       = aws_iam_role.ecs_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_execution_role_logs_policy_attachment" {
+  role       = aws_iam_role.ecs_execution_role.name
+  policy_arn = aws_iam_policy.ecs_execution_role_logs_policy.arn
 }
 
 resource "aws_ecs_cluster" "this" {
@@ -82,6 +128,33 @@ resource "aws_ecs_task_definition" "this" {
   # task_role_arn = ""
   execution_role_arn = aws_iam_role.ecs_execution_role.arn
 }
+
+resource "aws_ecs_service" "this" {
+  name            = "${local.naming_prefix}-ecs-svc"
+  cluster         = aws_ecs_cluster.this.id
+  launch_type     = "FARGATE"
+  task_definition = aws_ecs_task_definition.this.arn
+  desired_count   = 1
+  depends_on      = [aws_iam_role.ecs_execution_role]
+
+  # load_balancer {
+  #   target_group_arn = aws_lb_target_group.this.arn
+  #   container_name   = local.naming_prefix
+  #   container_port   = var.container_port
+  # }
+
+  network_configuration {
+    subnets          = ["subnet-0b48a9de2e556576a", "subnet-081c0813a049c90ad"]
+    security_groups  = [aws_security_group.ecs_sg.id]
+    assign_public_ip = false
+  }
+
+  lifecycle {
+    ignore_changes = [desired_count]
+  }
+}
+
+# TODO: Configure service auto-scaling
 
 ##########################
 # Elastic Load Balancer #
--- infra/variables.tf
+++ infra/variables.tf
@@ -22,10 +22,12 @@ variable "host_port" {
   default = 3000
 }
 variable "container_command" {
-  type    = list(string)
+  type = list(string)
+  # default = []
   default = ["/bin/sh -c \"echo '<html> <head> <title>Amazon ECS Sample App</title> <style>body {margin-top: 40px; background-color: #333;} </style> </head><body> <div style=color:white;text-align:center> <h1>Amazon ECS Sample App</h1> <h2>Congratulations!</h2> <p>Your application is now running on a container in Amazon ECS.</p> </div></body></html>' >  /usr/local/apache2/htdocs/index.html && httpd-foreground\""]
 }
 variable "container_entry_point" {
-  type    = list(string)
+  type = list(string)
+  # default = []
   default = ["sh", "-c"]
 }
